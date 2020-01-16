2bbff8922b3aaf8e78537db0f032e132b2374b9c
twambolt <terry.wambolt38@gmail.com>
2020-01-16 15:28:50 -0800

    first commit

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    public static float xMax = 7.1f, xMin = -8.2f;
+    public static float yMax = 8.0f, yMin = -2.0f;
+    public static float zMax = 2.7f, zMin = -4.4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float stompSpeed = 11f;
+    public float playerheight = 7.24f;
+    //private float horizontalInput;
+    //private float verticalInput;
+    //private float stompInput;
+
+    private bool isStomping = false;
+
+    public Rigidbody rgdb;
+
+    public CharacterController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleStomp();
+        HandleMovement();
+    }
+
+    /// <summary>
+    /// Handles the player's stomp ability
+    /// </summary>
+    private void HandleStomp() {
+        if (Input.GetKeyDown(KeyCode.Space) && !isStomping)
+        {
+            isStomping = true;
+            charController.Move(Vector3.down * stompSpeed);
+        }
+        ResetStomp();
+        isStomping = false;
+    }
+
+
+
+    /// <summary>
+    /// Handles the player movement on the screen and input from the arrow keys
+    /// </summary>
+    private void HandleMovement()
+    {
+        if (!isStomping)
+        {
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            charController.Move(move * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void ResetStomp()
+    {
+        while (isStomping) {
+
+            if (transform.position.y >= playerheight) {
+                isStomping = false;
+                break;
+            }
+            charController.Move(Vector3.up * 0.1f);
+        }
+    }
+
+}
